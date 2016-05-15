d9c9248e7420259451258bb9b617eece48624570
David Whale <david@thinkingbinaries.com>
2016-05-15 18:52:33 +0100

    Removed legacy direct-to-SPI interface. This code now does FSK tx/rx and OOK tx from the same driver.

--- src/energenie/drv/spih_arduino.c
+++
@@ -1,6 +0,0 @@
-// placeholder for arduino SPI hardware driver
-//
-// This will be used to aid porting to the arduino.
-// The arduino has more predictable timing than the Raspberry Pi,
-// and thus it is a much better test suite platform.
-
