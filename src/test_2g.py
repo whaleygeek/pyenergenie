1680f00d4ae79d2b6601b8d26893fe9ef0d97210
David Whale <david@thinkingbinaries.com>
2021-04-14 15:02:07 +0100

    The standard MIHO009 class is now used in test_2g.py This is so that now the class is developed it is used throughout both the tester and setup_tool.

--- src/test_2g.py
+++ src/test_2g.py
@@ -9,20 +9,20 @@ import energenie
 import time
 import random
 
-BURST_SIZE = 22  # inner times * .027s (about 27ms per burst)
-BURST_COUNT = 2  # outer times
-HOLDOFF = 3
-RND_WINDOW_MS = 100
+# Configuration parameters for performance tuning
+BURST_SIZE = 22     # (default 22)  inner times * .027s (about 27ms per burst)
+BURST_COUNT = 2     # (default 2)   outer times
+HOLDOFF = 3         # 3 seconds between messages
+RND_WINDOW_MS = 100 # 0..100ms randomised timing
 
-class MYDEV(energenie.Devices.ENER002):
-    """Customised legacy device with overriden burst timing"""
-    def __init__(self, device_id, air_interface=None):
-        energenie.Devices.ENER002.__init__(self, device_id, air_interface)
-        self.radio_config.inner_times = BURST_SIZE
-        self.radio_config.outer_times = BURST_COUNT
+# Devices under test (2 sides of the same MiHO009 2gang light switch)
+LEFT = energenie.Devices.MIHO009((0x123456, 1))
+LEFT.radio_config.inner_times = BURST_SIZE
+LEFT.radio_config.outer_times = BURST_COUNT
 
-LEFT  = MYDEV((0x123456, 1))
-RIGHT = MYDEV((0x123456, 2))
+RIGHT = energenie.Devices.MIHO009((0x123456, 2))
+RIGHT.radio_config.inner_times = BURST_SIZE
+RIGHT.radio_config.outer_times = BURST_COUNT
 
 def do_left(f):
     """Put left side into a specific state"""
