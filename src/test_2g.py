dfabe6ebb5581adf40d13951f0e004ae4dbeedf4
David Whale <david@thinkingbinaries.com>
2021-04-16 16:04:50 +0100

    Tested 2gang switch for 1 hour with 3 seconds pre silence and the given burst parameters. No adverse switching effects observed.

--- src/test_2g.py
+++ src/test_2g.py
@@ -12,19 +12,19 @@ import random
 # Configuration parameters for performance tuning
 #BURST_SIZE = 22     # (default 22)  inner times * .027s (about 27ms per burst)
 #BURST_COUNT = 2     # (default 2)   outer times
-#PRE_SILENCE = 3     # 3 seconds between messages (now in MIHO009)
+PRE_SILENCE = 2      # seconds between messages (now in MIHO009)
 RND_WINDOW_MS = 100  # 0..100ms randomised timing
 
 # Devices under test (2 sides of the same MiHO009 2gang light switch)
 LEFT = energenie.Devices.MIHO009((0x123456, 1))
 #LEFT.radio_config.inner_times = BURST_SIZE
 #LEFT.radio_config.outer_times = BURST_COUNT
-#LEFT.radio_config.tx_pre_silence = PRE_SILENCE
+LEFT.radio_config.tx_pre_silence = PRE_SILENCE
 
 RIGHT = energenie.Devices.MIHO009((0x123456, 2))
 #RIGHT.radio_config.inner_times = BURST_SIZE
 #RIGHT.radio_config.outer_times = BURST_COUNT
-#RIGHT.radio_config.tx_pre_silence = PRE_SILENCE
+RIGHT.radio_config.tx_pre_silence = PRE_SILENCE
 
 # Another device, to test inter-device interference (intentionaly same preamble)
 OOKDevice = energenie.Devices.ENER002((0x123456, 4))
@@ -57,18 +57,6 @@ def rnd_delay():
     d = float(d)/1000
     print("rnd_delay:%f" % d)
     time.sleep(d)
-
-#def interfere(state, delay):
-#    """Send interference messages on another device, for a time"""
-#    do_until = time.time() + delay
-#    #time.sleep(0.25)  # gap to allow OTHER to work
-#    while time.time() < do_until:
-#        if state:
-#            print("OTHER on")
-#            OTHER.turn_on()
-#        else:
-#            print("OTHER off")
-#            OTHER.turn_off()
 
 def cycle():
     """Do one cycle test of on/off of both switch sides"""
